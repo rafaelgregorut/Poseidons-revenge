4adc894636c895f1aa3f9826e28389a141df84c4
andre-yai <yai.andre@gmail.com>
2012-05-12 10:21:51 -0300

    Revert "Revert "MakeFile Fase 1""
    This reverts commit 5308b5b4464861b7c92b17284d0d31d3468bb5a7.

---
+++ fase1/barco.h
@@ -0,0 +1,7 @@
+float anguloIncidenciaVento(float velocidadeVentoX, float velocidadeVentoY);
+
+float anguloDirecaoBarco(float velocidadeBarcoX, float velocidadeBarcoY);
+float calculaAnguloFinal(float anguloDirecaoBarco,float anguloIncidenciaVento);
+
+float aceleracaoBarco(float velocidade, float angulo, float areaVela);
+
